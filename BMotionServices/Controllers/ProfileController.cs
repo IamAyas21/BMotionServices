cc382b9f57219b787b3216900319f523706c3047
IAM <iam.nurahman@gmail.com>
2020-08-03 20:00:21 +0700

    - Add Profile - Adjustment User

---
+++ BMotionServices/Controllers/ProfileController.cs
@@ -0,0 +1,56 @@
+﻿using BMotionServices.Entity;
+using BMotionServices.Models;
+using BMotionServices.Parse;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BMotionServices.Controllers
+{
+    //[Authorize]
+    [RoutePrefix("api/Profile")]
+    public class ProfileController : ApiController
+    {
+        BMotionDBEntities db = new BMotionDBEntities();
+        [HttpPost]
+        [Route("Profiles")]
+        public ResponsePurchaseHistory Profiles(PurchaseHistory historyItem)
+        {
+            List<PurchaseHistory> listHistory = new List<PurchaseHistory>();
+
+            try
+            {
+                var list = db.sp_HistorPerUser(historyItem.NIP).ToList();
+                foreach(var item in list)
+                {   
+                    historyItem = new PurchaseHistory();
+                    historyItem.OrderNo = item.OrderNo;
+                    historyItem.OutletNo = item.OutletNo;
+                    historyItem.TransactionDate = item.TransactionDate;
+                    historyItem.Liter = item.Liter;
+                    listHistory.Add(historyItem);
+                }
+
+                return new ResponsePurchaseHistory
+                {
+                    status = "success",
+                    message = "List Purchase History",
+                    Data = listHistory
+                };
+            }
+            catch (Exception e)
+            {
+                Logging.Log.getInstance().CreateLogError(e, JsonConvert.SerializeObject(listHistory));
+                return new ResponsePurchaseHistory
+                {
+                    status = "failed",
+                    message = e.Message
+                };
+            }
+        }
+    }
+}
---
+++ BMotionServices/Models/PurchaseHistory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BMotionServices.Models
+{
+    public class PurchaseHistory
+    {
+        string nip;
+        public String NIP
+        {
+            get { return nip; }
+            set { nip = value; }
+        }
+
+        string orderNo;
+        public String OrderNo
+        {
+            get { return orderNo; }
+            set { orderNo = value; }
+        }
+        string transactionDate;
+        public String TransactionDate
+        {
+            get { return transactionDate; }
+            set { transactionDate = value; }
+        }
+
+        string outletNo;
+        public String OutletNo
+        {
+            get { return outletNo; }
+            set { outletNo = value; }
+        }
+
+        string liter;
+        public String Liter
+        {
+            get { return liter; }
+            set { liter = value; }
+        }
+    }
+}
